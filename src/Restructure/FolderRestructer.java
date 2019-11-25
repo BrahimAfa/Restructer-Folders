87c5f255df90eca193afd1a6040a26f2aa09bb0b
Brahim Afassy <brahimafassy@gmail.com>
2019-11-26 00:22:58 +0100

    Checking if Folder Exist and if Is Valid

--- src/Restructure/FolderRestructer.java
+++ src/Restructure/FolderRestructer.java
@@ -1,5 +1,27 @@
 package Restructure;
 
-public class FolderRestructer {
+import java.io.File;
+import java.io.FileNotFoundException;
 
+public class FolderRestructer {
+	private String _DirectoryPath;
+	private File _File ;
+	
+	public FolderRestructer (String DirectoryPath) throws Exception {
+		this._DirectoryPath = DirectoryPath;
+		_File = new File(_DirectoryPath);
+		//throws Exception Of the The Given Path in Not a Directory
+		Check_Is_Directory_And_Valid_Path();
+		
+	}
+	 
+	private void Check_Is_Directory_And_Valid_Path() throws Exception  {
+		if(!_File.exists()) {
+			throw new FileNotFoundException("File Not Found!!! Check the if The Path is Valid");
+			
+		}
+		else if(!_File.isDirectory()) {
+			throw new Exception("The Given Path in Not a Directory");
+		}
+	}
 }
