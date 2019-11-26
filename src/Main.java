383f9248a19424ccab68cb2c43efd39523f1b954
Brahim Afassy <brahimafassy@gmail.com>
2019-11-26 01:39:37 +0100

    Chnged Main.java Messages to .formate() Function

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		FolderRestructer od = new FolderRestructer("C:\\Users\\brahi\\Downloads\\Opera Download");
+		FolderRestructer od = new FolderRestructer("Desired_File");
 		long time=System.currentTimeMillis();
 		od.RestructureDirectory();
-		System.out.println("<MAIN THREAD ENDED >Ended in : "+(System.currentTimeMillis()-time)+"ms");
-		System.out.println("<MAIN THREAD STARTED >START at : "+time+"ms");
+		System.out.format("<MAIN THREAD STARTED> STARTs at : %sms",time);
+		System.out.format("<MAIN THREAD ENDED> Ended in : %sms\n",(System.currentTimeMillis()-time));
 
-		System.out.println("Done");
+		System.out.println("<Main THREAD> Done ^.^");
 		
 		
 		//Tests done by calculating The LastThreadRunning(ms) - TimeTheFirstMainThreadRuns(ms)
 		//Experiment done on 68 FILES (images,docs,Videos,audio....) ===> 3.97GB
-		//copying Large file by 100MB and byte[100MB] ==> 4.07min
+		//copying Large file by 100MB and byte[100MB] ==> 4.07min "LOOL"
 		//copying Large file by 100MB and byte[1MB] ==>3.41min
 		//copying Large file by 250MB and byte[1MB] ==>2.71min
 		//copying Large file by 250MB and byte[100MB] ==>2.97min
